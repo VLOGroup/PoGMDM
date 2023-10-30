759a408facb21e5ac50a29ba493235f4ca2e6b4e
Martin Zach <martin.zach@icg.tugraz.at>
2023-10-30 17:45:29 +0100

    removed dead code to get rid of pad2d dependency (we never use padding)

--- conv.py
+++ conv.py
@@ -1,7 +1,6 @@
 import math
 
 import numpy as np
-import pad2d_op
 import torch as th
 
 __all__ = ['Conv2d', 'ConvScale2d', 'ConvScaleTranspose2d', 'Upsample2x2']
@@ -198,10 +197,6 @@ class Conv2d(th.nn.Module):
 
     def forward(self, x):
         weight = self.get_weight()
-        pad = weight.shape[-1] // 2
-        if self.pad and pad > 0:
-            # x = th.nn.functional.pad(x, (pad,pad,pad,pad), 'reflect')
-            x = pad2d_op.pad2d(x, [pad, pad, pad, pad], mode='reflect')
 
         return th.nn.functional.conv2d(
             x, weight, self.bias, self.stride, self.padding, self.dilation,
@@ -223,9 +218,6 @@ class Conv2d(th.nn.Module):
             x, weight, self.bias, self.stride, self.padding, output_padding,
             self.groups, self.dilation
         )
-        pad = weight.shape[-1] // 2
-        if self.pad and pad > 0:
-            x = pad2d_op.pad2dT(x, [pad, pad, pad, pad], mode='reflect')
         return x
 
     def extra_repr(self):
@@ -243,149 +235,3 @@ class Conv2d(th.nn.Module):
         if self.bound_norm:
             s += ", bound_norm={bound_norm}"
         return s.format(**self.__dict__)
-
-
-class ConvScale2d(Conv2d):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size=3,
-        invariant=False,
-        groups=1,
-        stride=2,
-        bias=False,
-        zero_mean=False,
-        bound_norm=False
-    ):
-        super(ConvScale2d, self).__init__(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            invariant=invariant,
-            stride=stride,
-            dilation=1,
-            groups=groups,
-            bias=bias,
-            zero_mean=zero_mean,
-            bound_norm=bound_norm
-        )
-
-        # create the convolution kernel
-        if self.stride > 1:
-            np_k = np.asarray([1, 4, 6, 4, 1], dtype=np.float32)[:, np.newaxis]
-            np_k = np_k @ np_k.T
-            np_k /= np_k.sum()
-            np_k = np.reshape(np_k, (1, 1, 5, 5))
-            self.register_buffer('blur', th.from_numpy(np_k))
-
-    def get_weight(self):
-        weight = super().get_weight()
-        if self.stride > 1:
-            weight = weight.reshape(-1, 1, self.kernel_size, self.kernel_size)
-            for i in range(self.stride // 2):
-                weight = th.nn.functional.conv2d(weight, self.blur, padding=4)
-            weight = weight.reshape(
-                self.out_channels, self.in_channels,
-                self.kernel_size + 2 * self.stride,
-                self.kernel_size + 2 * self.stride
-            )
-        return weight
-
-
-class ConvScaleTranspose2d(ConvScale2d):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size=3,
-        invariant=False,
-        groups=1,
-        stride=2,
-        bias=False,
-        zero_mean=False,
-        bound_norm=False
-    ):
-        super(ConvScaleTranspose2d, self).__init__(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            invariant=invariant,
-            groups=groups,
-            stride=stride,
-            bias=bias,
-            zero_mean=zero_mean,
-            bound_norm=bound_norm
-        )
-
-    def forward(self, x, output_shape):
-        return super().backward(x, output_shape)
-
-    def backward(self, x):
-        return super().forward(x)
-
-
-class Upsample2x2(Conv2d):
-    def __init__(
-        self, in_channels, out_channels, zero_mean=False, bound_norm=False
-    ):
-        super(Upsample2x2, self).__init__(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=1,
-            invariant=False,
-            groups=1,
-            stride=1,
-            bias=None,
-            zero_mean=zero_mean,
-            bound_norm=bound_norm
-        )
-
-        self.in_channels = in_channels
-
-        # define the interpolation kernel for 2x2 upsampling
-        np_k = np.asarray([1, 4, 6, 4, 1], dtype=np.float32)[:, np.newaxis]
-        np_k = np_k @ np_k.T
-        np_k /= np_k.sum()
-        np_k *= 4
-        np_k = np.reshape(np_k, (1, 1, 5, 5))
-        np_k = np.tile(np_k, (self.in_channels, 1, 1, 1))
-        self.register_buffer('kernel', th.from_numpy(np_k))
-
-    def forward(self, x, output_shape=None):
-
-        pad = self.kernel.shape[-1] // 2 // 2
-
-        # determine the amount of padding
-        if not output_shape is None:
-            output_padding = (
-                output_shape[2] - ((x.shape[2] - 1) * 2 + 1),
-                output_shape[3] - ((x.shape[3] - 1) * 2 + 1)
-            )
-        else:
-            output_padding = 0
-
-        x = optoth.pad2d.pad2d(x, (pad, pad, pad, pad), mode='symmetric')
-        # apply the upsampling kernel
-        x = th.nn.functional.conv_transpose2d(
-            x,
-            self.kernel,
-            stride=2,
-            padding=4 * pad,
-            output_padding=output_padding,
-            groups=self.in_channels
-        )
-        # apply the 1x1 convolution
-        return super().forward(x)
-
-    def backward(self, x):
-        # 1x1 convolution backward
-        x = super().backward(x)
-        # upsampling backward
-        pad = self.kernel.shape[-1] // 2 // 2
-        x = th.nn.functional.conv2d(
-            x, self.kernel, stride=2, padding=4 * pad, groups=self.in_channels
-        )
-        return optoth.pad2d.pad2d_transpose(
-            x, (pad, pad, pad, pad), mode='symmetric'
-        )
